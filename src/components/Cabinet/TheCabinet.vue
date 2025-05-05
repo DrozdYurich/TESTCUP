82c168ebb9cb088a51fd0430e3c72e5c91a8e1b2
Drozdov Evgeny <drozdove912@gmail.com>
2025-05-05 16:48:15 +0200

    32

--- src/components/Cabinet/TheCabinet.vue
+++ src/components/Cabinet/TheCabinet.vue
@@ -2,10 +2,12 @@
   <div>
     <h2>Личный кабинет</h2>
     <RouterView></RouterView>
+    <TheSideBar />
     <TheToolBar />
   </div>
 </template>
 <script setup>
+import TheSideBar from "./TheSideBar.vue";
 import TheToolBar from "./TheToolBar.vue";
 </script>
 <style></style>
