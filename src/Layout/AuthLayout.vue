0b246adfef4daed4206b4717d8fd71b4208ddd01
Drozdov Evgeny <drozdove912@gmail.com>
2025-05-12 18:15:10 +0200

    cssauth

--- src/Layout/AuthLayout.vue
+++ src/Layout/AuthLayout.vue
@@ -1,6 +1,41 @@
 <template>
-  <RouterView></RouterView>
+  <RouterView v-slot="{ Component }">
+    <transition name="fade" mode="out-in">
+      <component :is="Component"></component>
+    </transition>
+  </RouterView>
 </template>
 
 <script setup></script>
-<style scoped></style>
+<style scoped>
+.fade-enter-active {
+  animation: in 0.4s ease;
+}
+.fade-leave-active {
+  animation: out 0.4s ease;
+}
+.fade-enter-from,
+.fade-leave-to {
+  opacity: 0;
+}
+@keyframes in {
+  from {
+    opacity: 0;
+    transform: translateX(-100px);
+  }
+  to {
+    opacity: 1;
+    transform: translateX(0);
+  }
+}
+@keyframes out {
+  from {
+    opacity: 1;
+    transform: translateX(0);
+  }
+  to {
+    opacity: 0;
+    transform: translateX(100px);
+  }
+}
+</style>
