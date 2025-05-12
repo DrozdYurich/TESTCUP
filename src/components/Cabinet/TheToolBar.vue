0a11e3262ce05f439da15f6f4c2be760216417f4
Drozdov Evgeny <drozdove912@gmail.com>
2025-05-12 16:36:51 +0200

    4

--- src/components/Cabinet/TheToolBar.vue
+++ src/components/Cabinet/TheToolBar.vue
@@ -1,14 +1,16 @@
 <template>
   <Toolbar
-    class="fixed bottom-0 left-0 w-11/12 z-50 bg-white toolbar-mobile p-0"
+    class="fixed left-0 w-11/12 z-50 bg-white toolbar-mobile p-0"
+    style="bottom: 20px"
   >
     <template #center>
       <Button
         v-for="item in flatMenuItems"
         :key="item.key"
-        class="py-2.5 px-1 btn"
+        class="py-2.5 px-1 btn flex flex-col items-center justify-center"
         severity="secondary"
         :label="item.label"
+        :icon="item.icon"
         text
         :class="{ active: activeBtn === item.key }"
         @click="handleMenuClick(item)"
@@ -64,8 +66,12 @@ function handleMenuClick(item) {
   background: #e0e7ff !important; /* Пример активного цвета */
   color: #1d4ed8 !important;
 }
+.p-toolbar {
+  border-bottom: none !important;
+}
 .p-toolbar-center {
   display: flex;
   justify-content: space-around;
+  align-items: flex-start;
 }
 </style>
