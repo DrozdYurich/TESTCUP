8ac6e46f3bbfd05bcc30ebdd65b953044b253569
Drozdov Evgeny <drozdove912@gmail.com>
2025-05-17 12:22:39 +0200

    1

--- src/views/Three.vue
+++ src/views/Three.vue
@@ -2,9 +2,15 @@
   <div class="container">
     <div class="card">
       <Accordion value="0">
-        <AccordionPanel v-for="acc in tabs" :value="acc.value">
-          <AccordionHeader>{{ acc.title }}</AccordionHeader>
-          <AccordionContent>
+        <AccordionPanel
+          v-for="acc in tabs"
+          :value="acc.value"
+          :style="{ background: 'blue' }"
+        >
+          <AccordionHeader :style="{ background: 'blue' }">{{
+            acc.title
+          }}</AccordionHeader>
+          <AccordionContent :style="{ background: 'blue' }">
             <p class="m-0">
               {{ acc.content }}
             </p>
@@ -32,46 +38,12 @@ const tabs = ref([
 ]);
 </script>
 
-<style scoped>
+<style>
 .container {
   width: 90vw;
   margin: auto;
 }
-.p-accordion.p-component {
-}
-/* Стили для всего аккордеона */
-.p-accordion {
-  border: 1px solid var(--border-color);
-  border-radius: 6px;
-}
-
-/* Стили для заголовков */
-.p-accordion-header {
-  background-color: var(--card-background-color);
-  color: var(--text-color);
-  padding: 1rem;
-  border-bottom: 1px solid var(--border-color);
-}
-
-.p-accordion-header:hover {
-  background-color: var(--card-background-color-hover);
-}
-
-/* Стили для активного заголовка */
-.p-accordion-header.p-highlight {
-  background-color: var(--primary-color);
-  color: white;
-}
-
-/* Стили для содержимого */
-.p-accordion-content {
-  padding: 1rem;
-  background-color: var(--background-color);
-  border-bottom: 1px solid var(--border-color);
-}
-
-/* Иконки стрелок */
-.p-accordion-header-icon {
-  color: var(--text-color);
+.p-accordioncontent-content {
+  background-color: blue;
 }
 </style>
